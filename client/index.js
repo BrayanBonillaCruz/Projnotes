074e24d5597ab2423e30a6478b461328149cff96
Brayan <jj_jkl@outlook.es>
2023-04-23 19:49:46 -0600

    🔑Adding Airbnb style guide

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
 // cargando los estilos
-import './styles/style.css'
-console.log("🎉 Estilos cargados correctamente 🎉");
+/* eslint-disable no-console */
+import './styles/style.css';
+
+console.log('🎉 Estilos cargados correctamente 🎉');
