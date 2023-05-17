2790ba7f8c1e36d44d290f5a95472bb614b2535f
Brayan <jj_jkl@outlook.es>
2023-05-17 08:28:43 -0600

    🏆Menus desplegables

--- server/app.js
+++ server/app.js
@@ -47,18 +47,15 @@ if (nodeEnviroment === 'development') {
     'webpack-hot-middleware/client?reload=true&timeout=1000',
     webpackConfig.entry,
   ];
+  // Agregar el plugin a la configuración de desarrollo
+  // de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
   // Creating the bundler
   const bundle = webpack(webpackConfig);
   // Enabling the webpack middleware
   app.use(
     webpackDevMiddleware(bundle, {
       publicPath: webpackConfig.output.publicPath,
-    })
-  );
-  // Enabling the webpack HMR
-  app.use(
-    webpackHotMiddleware(bundle, {
-      publicPath: webpackConfig.output.path,
     })
   );
   // Enabling the webpack HRM
