fc272bc606df53109ded3ad8f36ef8d2bfa96388
Brayan <jj_jkl@outlook.es>
2023-06-03 01:13:16 +0000

    🍔 Yup

--- server/domains/project/project.router.js
+++ server/domains/project/project.router.js
@@ -4,6 +4,11 @@ import { Router } from 'express';
 // Importando el controlador
 import projectController from './project.controller';
 
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import projectValidator from './project.validator';
+
 // Creando una isntancia del enrutador
 const router = new Router();
 
@@ -16,6 +21,16 @@ router.get('/add', projectController.add);
 
 // POST "/project/add"
 router.post('/add', projectController.addPost);
+
+// POST "/project/add"
+router.post(
+  '/add',
+  ValidateFactory({
+    schema: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addPost
+);
 
 // Exporto este tramo de ruta
 export default router;
